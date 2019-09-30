3b343ded0451378bd97d21d861b1b2547d962457
Tanveer Salim <Tanveer.Salim@ttu.edu>
2019-09-30 01:44:09 -0500

    Updated Plan To Protect Against Stack Frame Randomization-Weaknesses + Return-to-libc explanation paper

---
+++ ads.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>
+<html xmlns='http://www.w3.org/1999/xhtml'>
+<head>
+	<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+	<META HTTP-EQUIV='CACHE-CONTROL' CONTENT='NO-CACHE'>
+	<meta name='rating' content='general'>
+	<link rel='stylesheet' href='/menu.css' type='text/css' media='screen' />
+	<link href='/rss/index.php' rel='alternate' type='application/rss+xml' title='News' />
+	<title>Library Genesis</title>
+	<!--[if IE 6]>
+	<style>
+		body {behavior: url('/csshover3.htc');}
+		#menu li .drop {background:url('img/drop.gif') no-repeat right 8px; 
+	</style>
+	<![endif]-->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-145683333-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-145683333-1');
+</script><script src="../clipboard.min.js"></script></head><body><script>
+	$(document).ready(function() {
+		function appendMessage(argument) {
+			var div = $('<div>').attr('id', 'message').text('Ad block is installed and active. Please support us by disabling it.');
+			var add = $('span').before(div);
+		}
+		setTimeout(function(){
+			if($("ins").css('display') == "none") {
+				appendMessage();
+			}
+		}, 500);
+	});
+	</script><span></span><table id=main  align="center" border=0>
+	<tr>
+
+	<td align="left" valign="top" bgcolor="#F5F6CE" width=200 nowrap></td>
+	<td align="center" valign="top" bgcolor="#A9F5BC"><a href="http://booksdl.org/get.php?md5=301b4fdbf8a1e0641882e2b55b8bb656&key=TDVPNJN0YULJGBCT"><h2>GET</h2></a></td>
+	<td align="left" valign="top" bgcolor="#F5F6CE" width=450></td>
+	</tr>
+	<tr>
+
+	<td bgcolor="#F5F6CE" valign=top><script async="async" data-cfasync="false" src="//native.propellerclick.com/1?z=2655390"></script></td>
+	<td><a href='/book/index.php?md5=301b4fdbf8a1e0641882e2b55b8bb656&oftorrent='>Download via torrent </a><input id="textarea-example" value="Harvey M. Deitel, Paul Deitel, David R. Choffnes-Operating Systems-Prentice Hall (2003).pdf" type="text" size="9"><button class="btn-clipboard" data-clipboard-target="#textarea-example"> (need rename file)</button><script>new Clipboard(".btn-clipboard");</script><table><tr><td><img src='/covers/1402000/301b4fdbf8a1e0641882e2b55b8bb656-d.png' border=0 width=240 style='padding: 5px'></td><td></td></tr><tr><td>Title: Operating Systems<br>
+Author(s): Harvey M. Deitel, Paul Deitel, David R. Choffnes<br>
+Publisher: Prentice Hall<br>
+Year: 2003<br>
+ISBN: 0131828274, 9780131828278<br></td><td><textarea rows='13' name='bibtext' id='bibtext' readonly cols='40'>@book{book:{1402607},
+   title =     {Operating Systems},
+   author =    {Harvey M. Deitel, Paul Deitel, David R. Choffnes},
+   publisher = {Prentice Hall},
+   isbn =      {0131828274, 9780131828278},
+   year =      {2003},
+   series =    {},
+   edition =   {3},
+   volume =    {},
+   url =       {http://gen.lib.rus.ec/book/index.php?md5=301b4fdbf8a1e0641882e2b55b8bb656}}</textarea></td></tr><tr><td colspan=2><p style='text-align:center'>
+<a href='https://www.worldcat.org/search?qt=worldcat_org_bks&q=Operating%20Systems&fq=dt%3Abks'>Search in WorldCat</a> 
+<a href='https://www.goodreads.com/search?utf8=✓&query=Operating%20Systems'>Search in Goodreads</a><br>
+<a href='https://www.abebooks.com/servlet/SearchResults?tn=Operating%20Systems&pt=book&cm_sp=pan-_-srp-_-ptbook'>Search in AbeBooks</a></td></tr></table></td>
+	<td bgcolor="#F5F6CE" valign=top><script async="async" data-cfasync="false" src="//native.propellerclick.com/1?z=2655389"></script></td>
+	</tr>
+	<tr>
+	<td></td>
+	<td></td>
+	<td></td>
+	
+	</tr>
+	<tr><td></td><td colspan=2>The third edition of Operating Systems has been entirely updated to reflect current core operating system concepts and design considerations. To complement the discussion of operating system concepts, the book features two in-depth case studies on Linux and Windows XP. The case studies follow the outline of the book, so readers working through the chapter material can refer to each case study to see how a particular topic is handled in either Linux or Windows XP. Using Java code to illustrate key points, Operating Systems introduces processes, concurrent programming, deadlock and indefinite postponement, mutual exclusion, physical and virtual memory, file systems, disk performance, distributed systems, security and more. New to this edition are a chapter on multithreading and extensive treatments of distributed computing, multiprocessing, performance, and computer security. An ideal up-to-date book for beginner operating systems readers.</td></tr><script type="text/javascript" src="//deloplen.com/apu.php?zoneid=2651327" async data-cfasync="false"></script>
+	</table></body></html>
